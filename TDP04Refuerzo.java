3ca64cb076a01ad460d5e3f28ee2bf74e8fe225e
joseluisjulca12 <165939687+joseluisjulca12@users.noreply.github.com>
2024-04-20 19:54:45 -0500

    Add files via upload

---
+++ TDP04Refuerzo.java
@@ -0,0 +1,10 @@
+package tdp04.refuerzo;
+
+public class TDP04Refuerzo {
+
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDO AL TALLER DE REFUERZO");
+        new Ejercicios().ejercicio3();
+    }
+    
+}
